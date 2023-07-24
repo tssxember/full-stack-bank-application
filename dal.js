865b5dd1266e2cf02ad502fcaa520ed54058aeaa
tssxember <34081710+tssxember@users.noreply.github.com>
2023-07-23 21:19:37 -0400

    updated dal file with remote db

--- dal.js
+++ dal.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+//const url         = 'mongodb://localhost:27017';
 
-//const url       = 'mongodb+srv://user1:testpassword@cluster0.3xfr9x4.mongodb.net/';
+const url       = 'mongodb+srv://user1:testpassword@cluster0.3xfr9x4.mongodb.net/';
 let db            = null;
  
 // connect to mongo
